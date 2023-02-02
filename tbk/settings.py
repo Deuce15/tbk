434952cbd46a94d8f25f66ee5708a54125b29742
Deuce15 <i.nyamu5@gmail.com>
2023-02-02 12:38:19 +0300

    Edited Static routes

--- tbk/settings.py
+++ tbk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!_0x%)&jas653d4s*sxx4glwc-menznv(kozh@wre+)z@2^5pd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['147.182.206.205']
+ALLOWED_HOSTS = ['147.182.206.205', 'localhost']
 
 
 # Application definition
@@ -128,9 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
